44d205edeccd12d6e24a386f73425fa4367eb6d1
Jonathan Colmenares <sansistypinggit@gmail.com>
2019-06-18 17:41:52 -0400

    download newmusic

--- src/minecraft/me/jonathing/minecraft/resourcefixer/ResourceFixer.java
+++ src/minecraft/me/jonathing/minecraft/resourcefixer/ResourceFixer.java
@@ -16,6 +16,44 @@ public class ResourceFixer
 		downloadResource("https://resources.download.minecraft.net/14/14ae57a6bce3d4254daa8be2b098c2d99743cc3f", "/music/calm3.ogg");
 	}
 	
+	public static void newmusic()
+	{
+		downloadResource("https://resources.download.minecraft.net/df/df1ff11b79757432c5c3f279e5ecde7b63ceda64", "/newmusic/hal1.ogg");
+		downloadResource("https://resources.download.minecraft.net/ce/ceaaaa1d57dfdfbb0bd4da5ea39628b42897a687", "/newmusic/hal2.ogg");
+		downloadResource("https://resources.download.minecraft.net/dd/dd85fb564e96ee2dbd4754f711ae9deb08a169f9", "/newmusic/hal3.ogg");
+		downloadResource("https://resources.download.minecraft.net/5e/5e7d63e75c6e042f452bc5e151276911ef92fed8", "/newmusic/hal4.ogg");
+		downloadResource("https://resources.download.minecraft.net/cc/cc1a80b6becfc6d6489c069f053d0a8adc71c339", "/newmusic/nuance1.ogg");
+		downloadResource("https://resources.download.minecraft.net/78/78152dc42b036e0de7616e6e669ce5a81d17f82a", "/newmusic/nuance2.ogg");
+		downloadResource("https://resources.download.minecraft.net/0f/0f374764aafba398f4ba90dba05a53a9e6166138", "/newmusic/piano1.ogg");
+		downloadResource("https://resources.download.minecraft.net/98/98a918eb2694defd19726436d4b9dca9e7dd6a65", "/newmusic/piano2.ogg");
+		downloadResource("https://resources.download.minecraft.net/95/9528b6a76e7bac64ca1145cc682e8a8448cc04e5", "/newmusic/piano3.ogg");
+	}
+	
+	public static void newsound()
+	{
+		
+	}
+	
+	public static void pe()
+	{
+		
+	}
+	
+	public static void sound()
+	{
+		
+	}
+	
+	public static void sound3()
+	{
+		
+	}
+	
+	public static void streaming()
+	{
+		
+	}
+	
 	/**
 	 * Method used to download a file using Java I/O library.
 	 * @author Lalit Bhagtani from Code Destine
